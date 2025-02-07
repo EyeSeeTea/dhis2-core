b3acd4337a3e244635d8127c5fa00a92d529489d
netroms <msvanaes@dhis2.org>
2025-02-07 14:00:01 +0800

    feat: expose allowed 2FA types in the configuration endpoint
    * feat: expose allowed 2FA types in the configuration endpoint
    
    (cherry picked from commit 7eb63810b067e7f594ac442c21401f67a6a435d4)

--- dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/ConfigurationController.java
+++ dhis-2/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/ConfigurationController.java
@@ -30,6 +30,7 @@ package org.hisp.dhis.webapi.controller;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
@@ -42,6 +43,7 @@ import org.hisp.dhis.common.OpenApi;
 import org.hisp.dhis.configuration.Configuration;
 import org.hisp.dhis.configuration.ConfigurationService;
 import org.hisp.dhis.dataelement.DataElementGroup;
+import org.hisp.dhis.external.conf.ConfigurationKey;
 import org.hisp.dhis.external.conf.DhisConfigurationProvider;
 import org.hisp.dhis.feedback.NotFoundException;
 import org.hisp.dhis.indicator.IndicatorGroup;
@@ -471,6 +473,15 @@ public class ConfigurationController {
   @GetMapping("/appHubUrl")
   public @ResponseBody String getAppHubUrl(Model model, HttpServletRequest request) {
     return appManager.getAppHubUrl();
+  }
+
+  @GetMapping("/twoFactorMethods")
+  public @ResponseBody Map<String, Boolean> getTwoFactorMethods() {
+    return Map.of(
+        "email2faEnabled",
+            Boolean.parseBoolean(config.getProperty(ConfigurationKey.EMAIL_2FA_ENABLED)),
+        "totp2faEnabled",
+            Boolean.parseBoolean(config.getProperty(ConfigurationKey.TOTP_2FA_ENABLED)));
   }
 
   /**
