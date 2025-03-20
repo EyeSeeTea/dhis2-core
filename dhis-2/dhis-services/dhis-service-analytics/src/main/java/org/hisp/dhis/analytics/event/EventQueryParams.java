2a947a7799ff401f920e0b5c70b366ee9ed538d3
Arnau Sanchez <pyarnau@gmail.com>
2025-03-20 12:31:02 +0100

    fix: add programStage param to getKey

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/EventQueryParams.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/event/EventQueryParams.java
@@ -403,6 +403,7 @@ public class EventQueryParams
         return key
             .addIgnoreNull( "value", value, () -> value.getUid() )
             .addIgnoreNull( "programIndicator", programIndicator, () -> programIndicator.getUid() )
+            .addIgnoreNull( "programStage", programStage, () -> programStage.getUid() )
             .addIgnoreNull( "organisationUnitMode", organisationUnitMode )
             .addIgnoreNull( "page", page )
             .addIgnoreNull( "pageSize", pageSize )
