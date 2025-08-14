5b5d8e617c11384429a745e9ef5aeb686d9350e7
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-08-14 20:09:08 +0800

    revert delete
    (cherry picked from commit f2f92607577f7e8f6ff398e1e3d5258813c52e8e)

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datavalue/DefaultDataValueService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/datavalue/DefaultDataValueService.java
@@ -203,9 +203,6 @@ public class DefaultDataValueService implements DataValueService {
               CurrentUserUtil.getCurrentUsername(),
               ChangeLogType.DELETE);
 
-      new DataValueAudit(
-              dataValue, dataValue.getValue(), dataValue.getStoredBy(), ChangeLogType.CREATE);
-
       dataValueAuditService.addDataValueAudit(dataValueAudit);
     }
 
