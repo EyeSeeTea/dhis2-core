80f9440facb027666fb95bae7adeafdc7ba30c95
Matias Arriola <matias@eyeseetea.com>
2025-08-25 12:52:38 -0300

    fix: Prevent duplicates after full analytics as part of the continuous analytics job by removing data from latest partition after each table run
    (cherry picked from commit defd838aab481235d952537e736c3a847296c2e1)

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/AbstractJdbcTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/AbstractJdbcTableManager.java
@@ -383,7 +383,11 @@ public abstract class AbstractJdbcTableManager implements AnalyticsTableManager 
     Date lastLatestPartitionUpdate =
         systemSettingManager.getDateSetting(
             SettingKey.LAST_SUCCESSFUL_LATEST_ANALYTICS_PARTITION_UPDATE);
-    Date lastAnyTableUpdate = DateUtils.getLatest(lastLatestPartitionUpdate, lastFullTableUpdate);
+
+    Date lastAnyTableUpdate =
+        (params.getForcedStartDate() != null)
+            ? params.getForcedStartDate()
+            : DateUtils.getLatest(lastLatestPartitionUpdate, lastFullTableUpdate);
 
     Assert.notNull(
         lastFullTableUpdate,
