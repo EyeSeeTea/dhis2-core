05072c9d358538651836ab167ef6ba5a96cc95b7
Matias Arriola <matias@eyeseetea.com>
2025-09-02 12:10:13 -0300

    Revert "fix: remove programTEA and programStageDE from root level in metadata export"
    This reverts commit 520d2d1a8871f5933ae26829de8778748040510f.

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/DefaultMetadataExportService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/metadata/DefaultMetadataExportService.java
@@ -906,7 +906,9 @@ public class DefaultMetadataExportService implements MetadataExportService {
           SetMap<Class<? extends IdentifiableObject>, IdentifiableObject> metadata,
           ProgramTrackedEntityAttribute programTrackedEntityAttribute) {
     if (programTrackedEntityAttribute == null) return metadata;
+    metadata.putValue(ProgramTrackedEntityAttribute.class, programTrackedEntityAttribute);
     handleAttributes(metadata, programTrackedEntityAttribute);
+
     handleTrackedEntityAttribute(metadata, programTrackedEntityAttribute.getAttribute());
 
     return metadata;
@@ -965,6 +967,7 @@ public class DefaultMetadataExportService implements MetadataExportService {
           SetMap<Class<? extends IdentifiableObject>, IdentifiableObject> metadata,
           ProgramStageDataElement programStageDataElement) {
     if (programStageDataElement == null) return metadata;
+    metadata.putValue(ProgramStageDataElement.class, programStageDataElement);
 
     handleAttributes(metadata, programStageDataElement);
     handleDataElement(metadata, programStageDataElement.getDataElement());
